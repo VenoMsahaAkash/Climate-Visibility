6cd4071e2ffabb1ddd0de4de4070a51322bd7264
VenoMsahaAkash <asaha9330@gmail.com>
2026-05-31 17:42:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="A",
+    author="akash",
     author_email="asaha9330@mail",
     packages=find_packages(),
     install_requires=[],
